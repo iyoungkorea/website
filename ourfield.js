33fff361dbfc25ac2027461b95145396d6d15e77
iyoungkorea <youngkorea.d@gmail.com>
2025-06-17 14:35:28 +0900

    minor edit

--- ourfield.js
+++ ourfield.js
@@ -1,6 +1,8 @@
 const ourfield ={
     EN: `
-    <img src="images/field.png" style="width:100%; height: auto;">
-    <img src="images/field(1).png" style="width:100%; height: auto;">
-`
+    <div class="ourfield-box">
+    <img src="images/field.png">
+    <img src="images/field(1).png">
+    </div>
+    `
 };
