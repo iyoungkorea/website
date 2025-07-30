b006536e1acdbc1eee2faf7fa1d9a64ca4960b36
iyoungkorea <youngkorea.d@gmail.com>
2025-07-30 12:16:19 +0900

    meta words, 20->25

--- overview.js
+++ overview.js
@@ -3,7 +3,7 @@ const overview = {
       <h4 style="color:#4f86c1;" id="about-title2">A leading company in the domestic and global markets</h4>
       <p id="about-text">
         YoungKorea Co., Ltd. is a specialized company dedicated to the independent research, development, and manufacturing of advanced in-vehicle technologies, including TVs, monitors, cameras, black boxes, and navigation systems.
-        Founded with a commitment to advancing automotive innovation, our expert R&D team—comprising professionals with over 20 years of experience—ensures the highest standards of performance and quality across every stage of production.
+        Founded with a commitment to advancing automotive innovation, our expert R&D team—comprising professionals with over 25 years of experience—ensures the highest standards of performance and quality across every stage of production.
         Recognized both domestically and internationally, YoungKorea has earned the trust of buyers and consumers through its excellence in automotive AV and security solutions.
       </p>
       <img src="images/papers.png" style="width:90%; height: auto;">
@@ -22,7 +22,7 @@ const overview = {
       <h4 style="color:#4f86c1;" id="about-title2">국내외 시장을 선도하는 기업</h4>
       <p id="about-text">
         영코리아 주식회사는 차량용 TV, 모니터, 카메라, 블랙박스, 내비게이션 시스템 등 첨단 차량 기술의 독자적인 연구, 개발, 제조에 전념하는 전문 기업입니다.
-        자동차 기술의 진보에 대한 확고한 의지를 바탕으로 설립된 당사는, 20년 이상의 경험을 갖춘 전문가들로 구성된 연구개발팀을 통해 개발부터 생산까지 모든 과정에서 최고 수준의 성능과 품질을 보장합니다.
+        자동차 기술의 진보에 대한 확고한 의지를 바탕으로 설립된 당사는, 25년 이상의 경험을 갖춘 전문가들로 구성된 연구개발팀을 통해 개발부터 생산까지 모든 과정에서 최고 수준의 성능과 품질을 보장합니다.
         당사는 국내외 시장에서 그 기술력과 품질을 인정받아 고객과 바이어로부터 깊은 신뢰를 얻고 있습니다.
       </p>
       <img src="images/papers.png" style="width:90%; height: auto;">
@@ -41,7 +41,7 @@ const overview = {
       <h4 style="color:#4f86c1;" id="about-title2">国内外市場をリードする企業</h4>
       <p id="about-text">
         ヤングコリア株式会社は、車載用テレビ、モニター、カメラ、ドライブレコーダー、ナビゲーションシステムなどの先進的な車載技術を独自に研究・開発・製造する専門企業です。
-        自動車技術の進歩に貢献するという使命のもとに設立され、20年以上の経験を持つ専門チームにより、開発から製造のあらゆる段階で最高水準の性能と品質を保証しています。
+        自動車技術の進歩に貢献するという使命のもとに設立され、25年以上の経験を持つ専門チームにより、開発から製造のあらゆる段階で最高水準の性能と品質を保証しています。
         国内外での技術と品質に対する高い評価を受けており、バイヤーや消費者から信頼されています。
       </p>
       <img src="images/papers.png" style="width:90%; height: auto;">
@@ -60,7 +60,7 @@ const overview = {
       <h4 style="color:#4f86c1;" id="about-title2">在国内外市场具有领导地位的企业</h4>
       <p id="about-text">
         YoungKorea Co., Ltd. 是一家专注于独立研发、设计和制造先进车载技术的专业公司，涵盖电视、监视器、摄像头、行车记录仪及导航系统。
-        公司秉持推动汽车技术创新的理念，由一支拥有20年以上经验的研发专家团队提供支持，确保从研发到生产的每一阶段都达到最高品质标准。
+        公司秉持推动汽车技术创新的理念，由一支拥有25年以上经验的研发专家团队提供支持，确保从研发到生产的每一阶段都达到最高品质标准。
         凭借卓越的技术与品质，YoungKorea 赢得了国内外客户的信任。
       </p>
       <img src="images/papers.png" style="width:90%; height: auto;">
